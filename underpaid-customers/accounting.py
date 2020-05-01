9472d5c28a6d4e6df650d55ecdc65106b3ea795c
Cassandra Sutton <cassandrasutton@gmail.com>
2020-04-30 21:28:27 -0700

    Defined working function

--- underpaid-customers/accounting.py
+++ underpaid-customers/accounting.py
@@ -8,48 +8,15 @@ def get_customer_invoice_balance(path):
         words = line.split("|")
 
         customer_name = words[1]
-        melons_bought = words[2]
-        total_paid = words[3]
+        melons_bought = float(words[2])
+        total_paid = float(words[3])
+        expected_payment = melons_bought * melon_cost
+    
+        if expected_payment != total_paid:
+            print(f"{customer_name} paid ${total_paid}", f"expected ${expected_payment.}.")
+        
+    accounting_records.close()
+
+get_customer_invoice_balance("customer-orders.txt")
 
 
-
-
-
-
-
-
-customer1_expected = customer1_melons * melon_cost
-if customer1_expected != customer1_paid:
-    print(f"{customer1_name} paid ${customer1_paid:.2f},",
-          f"expected ${customer1_expected:.2f}"
-          )
-
-customer2_expected = customer2_melons * melon_cost
-if customer2_expected != customer2_paid:
-    print(f"{customer2_name} paid ${customer2_paid:.2f},",
-          f"expected ${customer2_expected:.2f}"
-          )
-
-customer3_expected = customer3_melons * melon_cost
-if customer3_expected != customer3_paid:
-    print(f"{customer3_name} paid ${customer3_paid:.2f},",
-          f"expected ${customer3_expected:.2f}"
-          )
-
-customer4_expected = customer4_melons * melon_cost
-if customer4_expected != customer4_paid:
-    print(f"{customer4_name} paid ${customer4_paid:.2f},",
-          f"expected ${customer4_expected:.2f}"
-          )
-
-customer5_expected = customer5_melons * melon_cost
-if customer5_expected != customer5_paid:
-    print(f"{customer5_name} paid ${customer5_paid:.2f},",
-          f"expected ${customer5_expected:.2f}"
-          )
-
-customer6_expected = customer6_melons * melon_cost
-if customer6_expected != customer6_paid:
-    print(f"{customer6_name} paid ${customer6_paid:.2f},",
-          f"expected ${customer6_expected:.2f}"
-          )
