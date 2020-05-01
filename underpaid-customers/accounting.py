3e9114f7b8d001acd8b01c233dce95943e878f84
Cassandra Sutton <cassandrasutton@gmail.com>
2020-04-30 21:31:16 -0700

    Added line number for easy reference

--- underpaid-customers/accounting.py
+++ underpaid-customers/accounting.py
@@ -7,14 +7,16 @@ def get_customer_invoice_balance(path):
         words = line.rstrip()
         words = line.split("|")
 
+        line_number = words[0]
         customer_name = words[1]
         melons_bought = float(words[2])
         total_paid = float(words[3])
         expected_payment = melons_bought * melon_cost
     
         if expected_payment != total_paid:
-            print(f"{customer_name} paid ${total_paid}", f"expected ${expected_payment.}.")
+            print(f"Ref line {line_number}, {customer_name} paid ${total_paid}", f"expected ${expected_payment}.")
         
+    
     accounting_records.close()
 
 get_customer_invoice_balance("customer-orders.txt")
